3ce45110de475f0dbf1a17df01aaf082644a4bf8
noook <me@neilrichter.com>
2021-11-20 22:29:00 +0100

    feat: added shiki serverside

--- src/Controller/ExerciseController.php
+++ src/Controller/ExerciseController.php
@@ -5,7 +5,11 @@ namespace App\Controller;
 use App\Entity\Course;
 use App\Entity\Exercise;
 use App\Repository\ExerciseRepository;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\MarkdownConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Spatie\CommonMarkShikiHighlighter\HighlightCodeExtension;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,9 +21,20 @@ class ExerciseController extends AbstractController
     #[ParamConverter('exercise', options: ['mapping' => ['exercise' => 'slug']])]
     public function detail(Course $course, Exercise $exercise): Response
     {
+
+        $environment = new Environment();
+        $environment
+            ->addExtension(new CommonMarkCoreExtension())
+            ->addExtension(new HighlightCodeExtension('github-light'));
+
+        $commonMarkConverter = new MarkdownConverter($environment);
+
+        $exerciseContent = $commonMarkConverter->convertToHtml($exercise->getContent());
+
         return $this->render('exercise/detail.html.twig', [
             'course' => $course,
             'exercise' => $exercise,
+            'content' => $exerciseContent,
         ]);
     }
 
